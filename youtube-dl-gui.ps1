8ce959c30f9459577f5e97779829e9853156a878
Richard <82099665+richrdb@users.noreply.github.com>
2021-05-13 15:37:13 +0200

    Update and rename Youtube-dl Grabber.ps1 to youtube-dl-gui.ps1

--- youtube-dl-gui.ps1
+++ youtube-dl-gui.ps1
@@ -1,4 +1,4 @@
-﻿echo "Youtube-dl Grabber"
+﻿echo "Youtube-dl GUI"
 $filename = Read-Host -Prompt "Gewünschter Dateiname"
 $url = Read-Host -Prompt "Video-URL"
 youtube-dl.exe -o $filename $url --retries infinite --fragment-retries infinite
