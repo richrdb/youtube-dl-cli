f823e3d046b545e8748c00741cb720ff76da3e7a
Richard <82099665+richrdb@users.noreply.github.com>
2021-05-13 15:25:14 +0200

    Added ps1 script

---
+++ Youtube-dl Grabber.ps1
@@ -0,0 +1,6 @@
+﻿echo "Youtube-dl Grabber"
+$filename = Read-Host -Prompt "Gewünschter Dateiname"
+$url = Read-Host -Prompt "Video-URL"
+youtube-dl.exe -o $filename $url --retries infinite --fragment-retries infinite
+echo "Video erfolgreich heruntergeladen!"
+pause
