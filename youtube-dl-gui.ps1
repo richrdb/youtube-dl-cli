e5932b1a6ed22a242227adfe2cdccdc98171ad33
Richard <82099665+richrdb@users.noreply.github.com>
2021-05-18 16:25:22 +0200

    Update youtube-dl-gui.ps1
    optimization

--- youtube-dl-gui.ps1
+++ youtube-dl-gui.ps1
@@ -1,6 +1,6 @@
-﻿echo "Youtube-dl GUI"
+﻿Write-Output "Youtube-dl GUI"
 $filename = Read-Host -Prompt "Gewünschter Dateiname"
 $url = Read-Host -Prompt "Video-URL"
 youtube-dl.exe -o $filename $url --retries infinite --fragment-retries infinite
-echo "Video erfolgreich heruntergeladen!"
-pause
+Write-Output "Video erfolgreich heruntergeladen!"
+Pause
