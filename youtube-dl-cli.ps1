5d2e37d7466f111371347f7f9cff65dd81215e50
Richard <82099665+richrdb@users.noreply.github.com>
2021-09-01 11:58:27 +0200

    renamed GUI to CLI and replaced DE with EN

---
+++ youtube-dl-cli.ps1
@@ -0,0 +1,6 @@
+﻿Write-Output "youtube-dl CLI"
+$filename = Read-Host -Prompt "Output file name"
+$url = Read-Host -Prompt "Video-URL"
+youtube-dl.exe -o $filename $url --retries infinite --fragment-retries infinite
+Write-Output "Process finished!"
+Pause
--- youtube-dl-gui.ps1
+++
@@ -1,6 +0,0 @@
-﻿Write-Output "Youtube-dl GUI"
-$filename = Read-Host -Prompt "Gewünschter Dateiname"
-$url = Read-Host -Prompt "Video-URL"
-youtube-dl.exe -o $filename $url --retries infinite --fragment-retries infinite
-Write-Output "Video erfolgreich heruntergeladen!"
-Pause
